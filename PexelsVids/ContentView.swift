5cc34c4c79bc9d0d4a24f91887e48dc0961aa12d
Aryan Jagarwal <ajbaggar@gmail.com>
2023-02-26 13:11:42 +0530

    Initial Commit

---
+++ PexelsVids/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PexelsVids
+//
+//  Created by Aryan Jagarwal on 26/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PexelsVids/PexelsVidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PexelsVidsApp.swift
+//  PexelsVids
+//
+//  Created by Aryan Jagarwal on 26/02/23.
+//
+
+import SwiftUI
+
+@main
+struct PexelsVidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
