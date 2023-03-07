4daccfe453222a96996e3e53e793302342519dab
Aryan Jagarwal <ajbaggar@gmail.com>
2023-03-07 08:48:18 +0530

    created video view

---
+++ PexelsVids/Views/VideoView.swift
@@ -0,0 +1,32 @@
+//
+//  VideoView.swift
+//  PexelsVids
+//
+//  Created by Aryan Jagarwal on 04/03/23.
+//
+
+import SwiftUI
+import AVKit
+
+struct VideoView: View {
+    var video: Video
+    @State private var player = AVPlayer()
+    
+    var body: some View {
+        VideoPlayer(player: player)
+            .edgesIgnoringSafeArea(.all)
+            .onAppear {
+                if let link =
+                    video.videoFiles.first?.link {
+                    player = AVPlayer(url: URL(string: link)!)
+                    player.play()
+                }
+            }
+    }
+}
+
+struct VideoView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoView(video: previewVideo)
+    }
+}
